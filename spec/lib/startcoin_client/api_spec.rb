703b919513b52ec091becf6ec533439063562bf1
mazikwyry <a.mazur@exlabs.co.uk>
2017-01-16 10:59:53 +0100

    add specs for proxy option

--- spec/lib/startcoin_client/api_spec.rb
+++ spec/lib/startcoin_client/api_spec.rb
@@ -3,16 +3,23 @@ require 'spec_helper'
 describe StartcoinClient::API do
   subject { StartcoinClient::API.new(:user => $user, :pass => $pass) }
 
-  it "should have default host, port, ssl" do
+  it "should have default host, port, ssl, proxy" do
     subject.host.should == 'localhost'
     subject.port.should == 8332
     subject.ssl?.should be_false
+    subject.proxy.should be_nil
   end
 
-  it "should accept host, port, ssl options" do
-    req = StartcoinClient::API.new(:user => $user, :pass => $pass, :host => 'example.com', :port => 1234, :ssl => true)
+  it "should accept host, port, ssl, proxy options" do
+    req = StartcoinClient::API.new(:user => $user,
+                                   :pass => $pass,
+                                   :host => 'example.com',
+                                   :port => 1234,
+                                   :ssl => true,
+                                   :proxy => 'proxy.com')
     req.host.should == 'example.com'
     req.port.should == 1234
+    req.proxy.should == 'proxy.com'
     req.ssl?.should be_true
   end
 
@@ -23,6 +30,7 @@ describe StartcoinClient::API do
       :host => 'localhost',
       :port => 8332,
       :ssl => false,
+      :proxy => nil
     }
   end
 end
